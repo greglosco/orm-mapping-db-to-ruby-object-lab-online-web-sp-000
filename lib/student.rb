de6e6b87aba0c863fc51fcb11c02cbfc9d4603c2
Gregory Losco <gtlosco@gmail.com>
2019-08-17 22:21:53 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -15,7 +15,7 @@ class Student
    FROM students
    SQL
    
-   DB[conn].execute(sql).map do |row|
+   DB[:conn].execute(sql).map do |row|
      self.new_from_db(row)
    end
    
@@ -29,7 +29,7 @@ class Student
     LIMIT 1
     SQL
     
-    DB[conn].execute(sql, name).map do |row|
+    DB[:conn].execute(sql, name).map do |row|
       self.new_from_db(row)
     end.first
   end
