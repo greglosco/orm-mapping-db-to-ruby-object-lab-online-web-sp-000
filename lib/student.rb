c49fc319617d7725507adf9d0f54209ff4a35c4a
Gregory Losco <gtlosco@gmail.com>
2019-08-17 22:18:09 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -16,14 +16,13 @@ class Student
    SQL
    
    DB[conn].execute(sql).map do |row|
-     
-   
+     self.new_from_db(row)
+   end
    
   end
 
   def self.find_by_name(name)
-    # find the student in the database given a name
-    # return a new instance of the Student class
+    
   end
   
   def save
