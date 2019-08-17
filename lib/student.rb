411d400b8b1f766f910030cb75329ca2056ca4d0
Gregory Losco <gtlosco@gmail.com>
2019-08-17 22:46:21 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -60,7 +60,7 @@ class Student
    
   end
   
-  def self.first_X_students_in_grade_10(number_of_students)
+  def self.first_X_students_in_grade_10(number)
     sql = <<-SQL
     SELECT *
     FROM students
